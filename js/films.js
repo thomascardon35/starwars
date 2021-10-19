cf123cdc773687a7f2300552de8a00a61cc416a8
Paul Emmanuel Ribardiere <paul.ribardiere@gmail.com>
2021-10-19 17:25:28 +0200

    modif fichier films.js

--- js/films.js
+++ js/films.js
@@ -1,7 +1,7 @@
 const megaBox = document.getElementById("megaBox");
 const donnée_base = document.getElementById("data_base");
 const nom_vaisseaux = document.getElementById("data_second");
-const espèce = document.getElementById("data_three");
+const espece = document.getElementById("data_three");
 
 const API_URL = "https://swapi.dev/api/films/";
 
@@ -9,13 +9,17 @@ async function getData(){
     const resp = await fetch(API_URL);
     const dataFilms = await resp.json();
 
+    let bigData = [];
 
-    for(let index = 0; index < dataFilms.count; index++){
+   
+
+ 
+  for(let index = 0; index < dataFilms.count; index++){
 
             
      // VAISSEAUX       
             
-            let starship = [...dataFilms.results[index].starships];   
+           let starship = [...dataFilms.results[index].starships];   
                 let war = [];
             
                 for(let i = 0; i < starship.length ; i++){
@@ -40,12 +44,15 @@ async function getData(){
 
             let wookie = (await Promise.all(people)).map(v => v.name);
 
-            
-
+        // FILMS    
+        const grosData = {...dataFilms.results[index]}
+         
+           bigData.push(grosData);
            
-         
-         
-
+        
+     
+  
+            
         megaBox.innerHTML +=`
 
         <div class="box" id="box">
@@ -53,25 +60,40 @@ async function getData(){
 
             <div class="data_base" id="data_base">
             
-            <p>Résumé : ${dataFilms.results[index].opening_crawl}</p>
-            <p>Réalisateur : ${dataFilms.results[index].director}</p>
-            <p>Producteur : ${dataFilms.results[index].producer}</p>
-            <p>Année de sortie : ${dataFilms.results[index].release_date}</p>
-            </div>
+                <p>Résumé : ${dataFilms.results[index].opening_crawl}</p>
+                <p>Réalisateur : ${dataFilms.results[index].director}</p>
+                <p>Producteur : ${dataFilms.results[index].producer}</p>
+                <p>Année de sortie : ${dataFilms.results[index].release_date}</p>
+            
             <div class="data_second" id="data_second">
-            <p>Vaisseaux : ${star} </p>
+                <p>Vaisseaux : ${star} </p>
             </div>
             <div class="data_three" id="data_three">
-            <p> Espèces : ${wookie}</p>
+                <p> Espèces : ${wookie}</p>
 
+            </div>
+            
             </div>
 
 
         </div>
+     
         `
         
+        
     }
-    
+    console.log(bigData);
+
+    const LIST_BUTTON = document.querySelectorAll('h2');
+       
+        for(let i = 0; i < LIST_BUTTON.length; i++){ 
+        LIST_BUTTON[i].addEventListener('click', function() {
+            let go = this.nextElementSibling
+            go.classList.toggle('active');
+        });
+    }
+
+
 
 }
 getData(API_URL);
