67fe56d569b912d0459a6c534a4f771b7d573ab1
MohamedKerkeb <kerkeb.moha@gmail.com>
2021-10-19 17:30:43 +0200

    mise ajour du fichier vehicles

--- js/vehicles.js
+++ js/vehicles.js
@@ -1,5 +1,111 @@
 const API_URL = 'https://swapi.dev/api/vehicles/';
 const MEGABOX = document.getElementById('megabox')
+const INPUT = document.getElementById('search')
+const BTNSEARCH = document.getElementById('searchBtn')
+
+const API_URLS = [
+    'https://swapi.dev/api/vehicles/', 
+    'https://swapi.dev/api/vehicles/?page=2',
+    'https://swapi.dev/api/vehicles/?page=3',
+    'https://swapi.dev/api/vehicles/?page=4'
+]
+
+const megaData = []
+const superMegaData =[]
+
+
+
+const getBigData = async () => {
+    try{
+        let allData = API_URLS.map(async (url) => {
+            const data = await fetch(url);
+            return data.json();
+        })
+
+        const getAllData = (await Promise.all(allData)).map(a => 
+                a.results.map(b => {
+                    return superMegaData.push(b)
+            })
+        )
+
+        console.log(superMegaData)
+
+        BTNSEARCH.addEventListener('click', () => {
+            //console.log(INPUT.value)
+            let search = INPUT.value
+            getSearch(search)
+        })
+
+        const getSearch = (search) => {
+            let res = superMegaData.filter(e => e.name == search)
+            console.log(res)
+        }
+        
+        
+
+        const afficher = () => {
+            // console.log('aff', superMegaData)
+            for(let i = 0; i < superMegaData.length; i++){
+                MEGABOX.innerHTML += `
+                <div class="box">
+                    <h2 id="${i}" class="title">${superMegaData[i].name}</h2>
+                    <div class="data_base">
+                        <ul>
+                            <li>model:${superMegaData[i].model }</li>
+                            <li>manufacturer: ${superMegaData[i].manufacturer}</li>
+                            <li>cost in credits: ${superMegaData[i].cost_in_credits}</li>
+                            <li>length: ${superMegaData[i].length}</li>
+                            <li>max atmosphering speed: ${superMegaData[i].max_atmosphering_speed}</li>
+                            <li>crew: ${superMegaData[i].crew}</li>
+                            <li>passengers: ${superMegaData[i].passengers}</li>
+                            <li>cargo capacity ${superMegaData[i].cargo_capacity}</li>
+                            <li>consumables: ${superMegaData[i].consumables}</li>
+                            <li>vehicles class: ${superMegaData[i].vehicle_class}</li>
+                        </ul>
+                        <div class="data_second" id="data_second">
+                            <h3>Films</h3>
+                            <ul>
+                                ${superMegaData[i].films.map(f => `<li>${f}</li>`)}
+                            </ul>
+                        </div>
+                        <div class="data_three" id="data_three">
+                            <h3>Pilots</h3>
+                            <ul>
+                                ${superMegaData[i].pilots.length != 0 ? superMegaData[i].pilots : 'Pas de Pilotes'}
+                            </ul>
+                        </div> 
+                    </div>
+                </div>
+            `
+                
+            }
+        }
+        
+        afficher()
+
+        let TITLE = document.querySelectorAll('h2');
+
+        for (let i = 0; i < TITLE.length; i++) {
+            TITLE[i].addEventListener("click", function() {
+                let res = this.nextElementSibling
+                res.classList.toggle("active")
+            
+                console.log(superMegaData[this.id])
+        });
+
+}   
+        
+        
+    } catch(error) {
+        console.error(error)    
+    }
+}
+
+
+
+getBigData()
+console.log('AFFICHAGE')
+//  afficher()
 
 const getData = async () => {
     try {
@@ -18,8 +124,8 @@ const getData = async () => {
 				return data.json();
 			});
 
-			const getPilots = (await Promise.all(dataPilots)).map((p) => `<li>${p.name}</li>`);
-			console.log(getPilots);
+			const getPilots = (await Promise.all(dataPilots)).map((p) => p.name);
+			// console.log(getPilots);
 
 			/*
 			 * Récuperer les films
@@ -30,51 +136,87 @@ const getData = async () => {
 				return data.json();
 			});
 
-			const getFilms = (await Promise.all(dataFilms)).map((f) => `<li>${f.title}</li>`);
+			const getFilms = (await Promise.all(dataFilms)).map((f) => f.title);
 
-            console.log(vehicle)
+            // console.log(getFilms)
+
+            const bigData = {...vehicle, films: [...getFilms], pilots: [...getPilots]}
+            
+            megaData.push(bigData)
+
             MEGABOX.innerHTML += `
-            <div class="box">
-                <h2>${vehicle.name}</h2>
-                <div class="data_base">
-                    <ul>
-                        <li>model:${vehicle.model }</li>
-                        <li>manufacturer: ${vehicle.manufacturer}</li>
-                        <li>cost in credits: ${vehicle.cost_in_credits}</li>
-                        <li>length: ${vehicle.length}</li>
-                        <li>max atmosphering speed: ${vehicle.max_atmosphering_speed}</li>
-                        <li>crew: ${vehicle.crew}</li>
-                        <li>passengers: ${vehicle.passengers}</li>
-                        <li>cargo capacity ${vehicle.cargo_capacity}</li>
-                        <li>consumables: ${vehicle.consumables}</li>
-                        <li>vehicle class: ${vehicle.vehicle_class}</li>
-                    </ul>
-                </div>
-                <div class="data_second" id="data_second">
-                </div>
-                <div class="data_three" id="data_three">
-                </div>
-                <div>
-            <h3>Films</h3>
-            <ul>
-              ${getFilms}
-            </ul>
-          </div>
-          <div>
-          <h3>Pilots</h3>
-            <ul>
-            ${getPilots.length != 0 ? getPilots : 'Pas de Pilotes'}
-            </ul>
-          </div>
-            </div>
-            `
-        }   
+                    <div class="box">
+                        <h2>${bigData.name}</h2>
+                        <div class="data_base">
+                            <ul>
+                                <li>model:${bigData.model }</li>
+                                <li>manufacturer: ${bigData.manufacturer}</li>
+                                <li>cost in credits: ${bigData.cost_in_credits}</li>
+                                <li>length: ${bigData.length}</li>
+                                <li>max atmosphering speed: ${bigData.max_atmosphering_speed}</li>
+                                <li>crew: ${bigData.crew}</li>
+                                <li>passengers: ${bigData.passengers}</li>
+                                <li>cargo capacity ${bigData.cargo_capacity}</li>
+                                <li>consumables: ${bigData.consumables}</li>
+                                <li>vehicles class: ${bigData.vehicle_class}</li>
+                            </ul>
+                        </div>
+                        <div class="data_second" id="data_second">
+                            <h3>Films</h3>
+                            <ul>
+                                ${bigData.films.map(f => `<li>${f}</li>`)}
+                            </ul>
+                        </div>
+                        <div class="data_three" id="data_three">
+                            <h3>Pilots</h3>
+                            <ul>
+                                ${bigData.pilots.length != 0 ? bigData  .pilots : 'Pas de Pilotes'}
+                            </ul>
+                        </div>  
+                    </div>
+                `
+        }
+    console.log(megaData)
+
     } catch (error) {
         console.error(error);
     }
-    
-
-
 }
 
-getData()
+
+
+// getData()
+
+
+        //     MEGABOX.innerHTML += `
+        //     <div class="box">
+        //         <h2>${vehicle.name}</h2>
+        //         <div class="data_base">
+        //             <ul>
+        //                 <li>model:${vehicle.model }</li>
+        //                 <li>manufacturer: ${vehicle.manufacturer}</li>
+        //                 <li>cost in credits: ${vehicle.cost_in_credits}</li>
+        //                 <li>length: ${vehicle.length}</li>
+        //                 <li>max atmosphering speed: ${vehicle.max_atmosphering_speed}</li>
+        //                 <li>crew: ${vehicle.crew}</li>
+        //                 <li>passengers: ${vehicle.passengers}</li>
+        //                 <li>cargo capacity ${vehicle.cargo_capacity}</li>
+        //                 <li>consumables: ${vehicle.consumables}</li>
+        //                 <li>vehicle class: ${vehicle.vehicle_class}</li>
+        //             </ul>
+        //         </div>
+        //         <div class="data_second" id="data_second">
+        //             <h3>Films</h3>
+        //             <ul>
+        //                 ${getFilms}
+        //             </ul>
+        //         </div>
+        //         <div class="data_three" id="data_three">
+        //             <h3>Pilots</h3>
+        //             <ul>
+        //                 ${getPilots.length != 0 ? getPilots : 'Pas de Pilotes'}
+        //             </ul>
+        //         </div>  
+        //     </div>
+        //     `
+        // }   
